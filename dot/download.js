f6e13d80c655ce289b54ca78d56633529ada79da
SY <97039512+Syanagisato@users.noreply.github.com>
2022-02-24 12:26:59 +0900

    Add files via upload

--- dot/download.js
+++ dot/download.js
@@ -1,9 +1,12 @@
 var dl = document.getElementById("dl");
 dl.addEventListener('click', () => {
-	html2canvas(document.querySelector("#canvas")).then(canvas => {
-		let dlEle = document.createElement("a");
-		dlEle.href = canvas.toDataURL("image/png");
-		dlEle.download = "canvas.png";
-		dlEle.click();
-	});
+	var con = confirm('ダウンロードしますか？');
+	if(con){
+		html2canvas(document.querySelector("#canvas")).then(canvas => {
+			let dlEle = document.createElement("a");
+			dlEle.href = canvas.toDataURL("image/png");
+			dlEle.download = "canvas.png";
+			dlEle.click();
+		});
+	}
 });
