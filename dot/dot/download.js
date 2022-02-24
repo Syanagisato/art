09f09a7c0134b7307b7cc1ef751fc83087ba21c9
SY <97039512+Syanagisato@users.noreply.github.com>
2022-02-24 11:44:30 +0900

    Delete dot directory

--- dot/dot/download.js
+++
@@ -1,9 +0,0 @@
-var dl = document.getElementById("dl");
-dl.addEventListener('click', () => {
-	html2canvas(document.querySelector("#canvas")).then(canvas => {
-		let dlEle = document.createElement("a");
-		dlEle.href = canvas.toDataURL("image/png");
-		dlEle.download = "canvas.png";
-		dlEle.click();
-	});
-});
