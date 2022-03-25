dbc79078530a8fc89342dd85e2fd5513e6042810
SY <97039512+Syanagisato@users.noreply.github.com>
2022-03-25 14:48:26 +0900

    Add files via upload

---
+++ nekocafe/add.php
@@ -0,0 +1,34 @@
+//予約情報の登録
+<?php
+session_start();
+
+if(empty($_POST["date"]) || empty($_POST["time"]) || empty($_POST["num"]) || empty($_POST["menu"]) || empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["phone"])) {
+	header("Location: yoyaku.php?err=1");
+	exit();
+}
+
+require_once("template/config.php");
+
+$date = $_POST["date"];
+$time = htmlspecialchars($_POST["time"], ENT_QUOTES);
+$num = $_POST["num"];
+$name = htmlspecialchars($_POST["name"], ENT_QUOTES);
+$email = htmlspecialchars($_POST["email"], ENT_QUOTES);
+$phone = htmlspecialchars($_POST["phone"], ENT_QUOTES);
+print_r($_POST);
+
+$sql = "INSERT INTO yoyaku(date, time, num, menu, name, email, phone)";
+$sql .= " VALUES(:date,:time, :num, :menu, :name, :email, :phone)";
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(":date", $date, PDO::PARAM_STR);
+$stmt->bindValue(":time", $time, PDO::PARAM_STR);
+$stmt->bindValue(":num", $num, PDO::PARAM_INT);
+$stmt->bindValue(":menu", $_POST["menu"], PDO::PARAM_STR);
+$stmt->bindValue(":name", $name, PDO::PARAM_STR);
+$stmt->bindValue(":email", $email, PDO::PARAM_STR);
+$stmt->bindValue(":phone", $phone, PDO::PARAM_STR);
+$stmt->execute();
+
+header("Location: confirm.php");
+?>
--- nekocafe/confirm.php
+++ nekocafe/confirm.php
@@ -1,15 +1,42 @@
 <?php
 session_start();
-if(empty($_POST["date"]) || empty($_POST["num"]) || empty($_POST["menu"]) || empty($_POST["uname"]) || empty($_POST["email"]) || empty($_POST["phone"])){
-	header("Location: yoyaku.php");
-	exit();
-}
-
-$date = $_POST["date"];
-$time = htmlspecialchars($_POST["time"], ENT_QUOTES);
-$num = $_POST["num"];
-$uname = htmlspecialchars($_POST["uname"], ENT_QUOTES);
-$email = htmlspecialchars($_POST["email"], ENT_QUOTES);
-$phone = htmlspecialchars($_POST["phone"], ENT_QUOTES);
-print_r($_POST);
+require_once('template/config.php');
+$sql = "SELECT date, time, num, menu, name, email, phone FROM yoyaku";
+$rs = $pdo->query($sql);
+$row = $rs->fetch(PDO::FETCH_ASSOC);
 ?>
+
+<!doctype html>
+<html lang="ja">
+
+<head>
+	<meta charset="utf-8">
+	<title>来店予約</title>
+	<link rel="stylesheet" href="css/style2.css">
+	<link rel="stylesheet" href="js/jquery-ui.css">
+	<meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+	<?php require_once("template/header.php"); ?>
+	
+	<h1>以下の内容で予約が完了しました！</h1>
+	<div id="naiyou">
+		<ul>
+			<li>来店日:<?php echo $row['date']; ?></li>
+			<li>来店時間:<?php echo $row['time']; ?></li>
+			<li>人数:<?php echo $row['num']; ?></li>
+			<li>メニュー:<?php echo $row['menu']; ?></li>
+			<li>おなまえ:<?php echo $row['name']; ?></li>
+			<li>メールアドレス:<?php echo $row['email']; ?></li>
+			<li>電話番号:<?php echo $row['phone']; ?></li>
+		</ul>
+	</div>
+	
+	<script src="js/jquery-3.6.0.min.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	
+	<?php require_once("template/footer.php"); ?>
+</body>
+
+</html>
